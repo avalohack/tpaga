1908e8ec6253208fe7f67d7da3150776d80215b1
avalohack <info@vacasenvuelo.com>
2019-06-10 17:46:57 -0500

    m

--- application/views/login.php
+++ application/views/login.php
@@ -8,6 +8,60 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 vista de usuario
 login
 
+
+<div class="container">
+  <div class="card-deck mb-3 text-center">
+		<div class="card mb-4 shadow-sm">      
+			<!--   <div class="card-header">
+			    <h4 class="my-0 font-weight-normal"><?php //echo $value['Name'] ?></h4>
+			  </div>
+			  <div class="card-body">
+			    <h1 class="card-title pricing-card-title"><?php //echo $value['Cost'] ?> <small class="text-muted">$</small></h1>
+			    <ul class="list-unstyled mt-3 mb-4">
+			      <li><?php //echo $value['Includ'] ?></li>
+			    </ul>
+			    <button onclick="AddProduct(<?php //echo  $value['IdPlans']; ?>)" type="button" class="btn btn-lg btn-block btn-primary">Agregar</button>
+			  </div> -->
+		</div>
+
+		<div class="card mb-4 shadow-sm">      
+			  <div class="card-header">
+			    <h4 class="my-0 font-weight-normal"><?php //echo $value['Name'] ?></h4>
+			  </div>
+			  <div class="card-body">
+			    <h1 class="card-title pricing-card-title"><?php //echo $value['Cost'] ?> <small class="text-muted">$</small></h1>
+			    <ul class="list-unstyled mt-3 mb-4">
+			      <li><?php //echo $value['Includ'] ?></li>
+			    </ul>
+			    <button onclick="AddProduct(<?php //echo  $value['IdPlans']; ?>)" type="button" class="btn btn-lg btn-block btn-primary">Agregar</button>
+			  </div>
+		</div>
+		<div class="card mb-4 shadow-sm">      
+			 <!--  <div class="card-header">
+			    <h4 class="my-0 font-weight-normal"><?php //echo $value['Name'] ?></h4>
+			  </div>
+			  <div class="card-body">
+			    <h1 class="card-title pricing-card-title"><?php //echo $value['Cost'] ?> <small class="text-muted">$</small></h1>
+			    <ul class="list-unstyled mt-3 mb-4">
+			      <li><?php //echo $value['Includ'] ?></li>
+			    </ul>
+			    <button onclick="AddProduct(<?php //echo  $value['IdPlans']; ?>)" type="button" class="btn btn-lg btn-block btn-primary">Agregar</button>
+			  </div> -->
+		</div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
  include 'include/footer.php';
  include 'include/end.php';
